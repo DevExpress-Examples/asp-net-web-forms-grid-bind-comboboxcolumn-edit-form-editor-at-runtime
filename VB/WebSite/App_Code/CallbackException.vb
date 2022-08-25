5befd910dc5c919c50f12c338c070d8eadc6f7e4
EugeniyBurmistrov <psycrab88@gmail.com>
2022-08-25 16:47:16 +0400

    Revert "Source auto update [skip ci]"
    This reverts commit a27274c2c020b1ceb6580daa47c915db732eb8ca.

---
+++ VB/WebSite/App_Code/CallbackException.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+
+''' <summary>
+''' Summary description for CallbackException
+''' </summary>
+Public Class CallbackException
+    Inherits Exception
+
+    Public Sub New(ByVal message As String)
+        MyBase.New(message)
+    End Sub
+End Class
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,33 @@
+﻿Option Infer On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        Dim comboColumn = (CType(grid.Columns("CategoryID"), GridViewDataComboBoxColumn))
+        comboColumn.PropertiesComboBox.DataSource = dsCombo
+        comboColumn.PropertiesComboBox.TextField = "CategoryName"
+        comboColumn.PropertiesComboBox.ValueField = "CategoryID"
+        comboColumn.PropertiesComboBox.ValueType = GetType(Int32)
+    End Sub
+
+    Protected Sub grid_RowInserting(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataInsertingEventArgs)
+        Throw New CallbackException("Data modifications are not allowed in the online example.")
+    End Sub
+    Protected Sub grid_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+        Throw New CallbackException("Data modifications are not allowed in the online example.")
+    End Sub
+    Protected Sub grid_CustomErrorText(ByVal sender As Object, ByVal e As ASPxGridViewCustomErrorTextEventArgs)
+        If TypeOf e.Exception Is CallbackException Then
+            e.ErrorText = e.Exception.Message
+        End If
+    End Sub
+End Class
